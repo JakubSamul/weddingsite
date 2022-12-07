749dec2b2cbd8b4c45c0bd2d5f3fe67f600d8eb4
Jakub Samul <jakubsamul0@gmail.com>
2022-12-07 15:58:07 +0100

    'cd'

--- love/views.py
+++ love/views.py
@@ -1,5 +1,27 @@
-from django.http import HttpResponse
+from django.http import HttpResponse, Http404
+from django.shortcuts import render, get_object_or_404
+from django.template import loader
+from .models import Question
 
 
 def index(request):
-    return HttpResponse("Witaj mistrzuniu !!!")
+    latest_question_list = Question.objects.order_by('-pub_date')[:5]
+    # template = loader.get_template('love/index.html')
+    context = {
+        'latest_question_list':latest_question_list,
+    }
+    return render(request, 'love/index.html', context)
+
+def detail(request, question_id):
+    return HttpResponse("You're looking at question %s." % question_id)
+
+def results(request, question_id):
+    response = "You're looking at the results of question %s."
+    return HttpResponse(response % question_id)
+
+def vote(request, question_id):
+    return HttpResponse("You're voting on question %s." % question_id)
+
+def detail(request, question_id):
+    question = get_object_or_404(Question, pk=question_id)
+    return render(request, 'love/detail.html', {'question':question})
