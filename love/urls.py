f2b180e9a52bc06e77f9210ae1ec7dc4c2a62f86
Jakub Samul <jakubsamul0@gmail.com>
2023-02-20 19:10:27 +0100

    zmiany

--- love/urls.py
+++ love/urls.py
@@ -1,4 +1,4 @@
-from django.urls import path
+from django.urls import path, reverse_lazy
 from django.views.generic import CreateView, UpdateView, DeleteView
 from .views import (
     GuestListView,
@@ -11,21 +11,29 @@ from .models import Guests
 urlpatterns = [
     path('', 
          HomePageView.as_view(), 
-         name='homepage'),
+         name = 'homepage'),
     path("login/", 
          LoginView.as_view(), 
-         name="login"),
+         name = "login"),
     path("logout/", 
          LogoutView.as_view(), 
-         name="logout"),
-    path('guest/', 
+         name = "logout"),
+    path('guestlist/add/', 
          CreateView.as_view(
-            model=Guests,
-            fields='__all__',
-            template_name='guests_form.html'
+            model = Guests,
+            fields = '__all__',
+            success_url = reverse_lazy('guests-list'),
+            template_name = 'guests_form.html'
          ), 
-         name='guest'),
+         name='guestslist-add'),
     path('guestslist/', 
          GuestListView.as_view(), 
-         name='guests-list')
+         name = 'guests-list'),
+     path('guestslist/<int:pk>/delete/',
+          DeleteView.as_view(
+               model = Guests,
+               success_url = reverse_lazy('guests_list'),
+               template_name = 'guests_delete.html'
+          ),
+          name='guestslist-delete')
 ]
